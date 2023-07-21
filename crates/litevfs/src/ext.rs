b67f4265db17c0ccfaa73b8b0d0c4f9f73094491
Pavel Borzenkov <pavel@fly.io>
2023-07-21 12:28:47 +0200

    litevfs: switch to dynamically loaded extension

--- crates/litevfs/src/ext.rs
+++ crates/litevfs/src/ext.rs
@@ -1,19 +1,22 @@
 use crate::LiteVfs;
 use log::{info, trace};
-use sqlite_vfs::{register, RegisterError};
-use std::ffi::{c_char, c_int};
-
-const SQLITE_OK_LOAD_PERMANENTLY: i32 = 256;
-const SQLITE_ERROR: i32 = 1;
+use sqlite_vfs::{ffi, register, RegisterError};
 
 #[no_mangle]
-pub extern "C" fn sqlite3_litevfs_init(_dummy: *const c_char) -> c_int {
+#[allow(non_snake_case)]
+pub extern "C" fn sqlite3_litevfs_init(
+    _db: *mut ffi::sqlite3,
+    _pzErrMsg: *mut *mut std::ffi::c_char,
+    pApi: *mut ffi::sqlite3_api_routines,
+) -> std::ffi::c_int {
     env_logger::init();
+
+    sqlite_vfs::init_extention(pApi);
 
     info!("registering LiteVFS");
     let code = match register("litevfs", LiteVfs::new("/tmp"), true) {
-        Ok(_) => SQLITE_OK_LOAD_PERMANENTLY,
-        Err(RegisterError::Nul(_)) => SQLITE_ERROR,
+        Ok(_) => ffi::SQLITE_OK_LOAD_PERMANENTLY,
+        Err(RegisterError::Nul(_)) => ffi::SQLITE_ERROR,
         Err(RegisterError::Register(code)) => code,
     };
     trace!("register(litevfs) -> {}", code);
